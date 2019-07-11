615db6f2ed216e0e84bee2bb55a0fc36b88cf627
Fernand Pajot <fernand@epigram.me>
2019-07-11 11:04:07 -0700

    Scaffolding in place.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 fn main() {
     let sdl = sdl2::init().unwrap();
     let video_subsystem = sdl.video().unwrap();
+    let gl_attr = video_subsystem.gl_attr();
+    gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
+    gl_attr.set_context_version(4, 6);
     let window = video_subsystem
         .window("Game", 900, 700)
         .opengl()
@@ -9,9 +12,6 @@ fn main() {
         .unwrap();
     let _gl_context = window.gl_create_context().unwrap();
     gl::load_with(|name| video_subsystem.gl_get_proc_address(name) as *const _);
-    unsafe {
-        gl::ClearColor(0.3, 0.3, 0.5, 1.0);
-    }
     let mut event_pump = sdl.event_pump().unwrap();
     'main: loop {
         for event in event_pump.poll_iter() {
@@ -21,8 +21,10 @@ fn main() {
             }
         }
         unsafe {
+            gl::ClearColor(0.3, 0.3, 0.5, 1.0);
             gl::Clear(gl::COLOR_BUFFER_BIT);
         }
         window.gl_swap_window();
+        std::thread::sleep(std::time::Duration::new(0, 1_000_000_000u32 / 60));
     }
 }
