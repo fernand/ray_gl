67ee61944c8b4abdbd0c757c11f5c2afd67a9284
Fernand Pajot <fernand@epigram.me>
2019-07-11 10:55:05 -0700

    Window works.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 fn main() {
     let sdl = sdl2::init().unwrap();
     let video_subsystem = sdl.video().unwrap();
-    let _window = video_subsystem
+    let window = video_subsystem
         .window("Game", 900, 700)
+        .opengl()
         .resizable()
         .build()
         .unwrap();
+    let _gl_context = window.gl_create_context().unwrap();
+    gl::load_with(|name| video_subsystem.gl_get_proc_address(name) as *const _);
+    unsafe {
+        gl::ClearColor(0.3, 0.3, 0.5, 1.0);
+    }
     let mut event_pump = sdl.event_pump().unwrap();
     'main: loop {
         for event in event_pump.poll_iter() {
@@ -14,5 +20,9 @@ fn main() {
                 _ => {}
             }
         }
+        unsafe {
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+        window.gl_swap_window();
     }
 }
