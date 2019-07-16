d9a03f29ce89949669b83e073ee3ce11eab6ee82
Fernand Pajot <fernand@epigram.me>
2019-07-16 09:06:28 -0700

    Removed SDL and OpenGL window stuff, as well as test shaders.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+#![allow(unused_imports)]
 mod gl_shader;
 
 use gl::types::{GLint, GLsizeiptr, GLuint, GLvoid};
@@ -11,86 +13,5 @@ fn main() {
     let gl_attr = video_subsystem.gl_attr();
     gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
     gl_attr.set_context_version(4, 6);
-    let window = video_subsystem
-        .window("Game", 900, 700)
-        .opengl()
-        .resizable()
-        .build()
-        .unwrap();
-    let _gl_context = window.gl_create_context().unwrap();
     gl::load_with(|name| video_subsystem.gl_get_proc_address(name) as *const _);
-    let mut event_pump = sdl.event_pump().unwrap();
-
-    let vert_shader = shader_from_source(
-        "triangle.vert",
-        &std::fs::read("shaders/triangle.vert").unwrap(),
-        ShaderKind::Vertex,
-    );
-    let frag_shader = shader_from_source(
-        "triangle.frag",
-        &std::fs::read("shaders/triangle.frag").unwrap(),
-        ShaderKind::Fragment,
-    );
-    let shader_program = ShaderProgram::from_shaders(&[vert_shader, frag_shader]);
-    let vertices: Vec<f32> = vec![
-        -0.5, -0.5, 0.0, 1.0, 0.0, 0.0,
-        0.5, -0.5, 0.0, 0.0, 1.0, 0.0,
-        0.0, 0.5, 0.0, 0.0, 0.0, 1.0];
-    let mut vbo: GLuint = 0;
-    let mut vao: GLuint = 0;
-    unsafe {
-        gl::GenBuffers(1, &mut vbo);
-        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
-        gl::BufferData(
-            gl::ARRAY_BUFFER,
-            (vertices.len() * mem::size_of::<f32>()) as GLsizeiptr,
-            vertices.as_ptr() as *const GLvoid,
-            gl::STATIC_DRAW,
-        );
-        gl::GenVertexArrays(1, &mut vao);
-        gl::BindVertexArray(vao);
-        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
-        gl::EnableVertexAttribArray(0);
-        gl::VertexAttribPointer(
-            0,
-            3,
-            gl::FLOAT,
-            gl::FALSE,
-            (6 * mem::size_of::<f32>()) as GLint,
-            ptr::null(),
-        );
-        gl::EnableVertexAttribArray(1);
-        gl::VertexAttribPointer(
-            1,
-            3,
-            gl::FLOAT,
-            gl::FALSE,
-            (6 * mem::size_of::<f32>()) as GLint,
-            (3 * mem::size_of::<f32>()) as *const GLvoid
-        );
-        gl::BindBuffer(gl::ARRAY_BUFFER, 0);
-        gl::BindVertexArray(0);
-    }
-
-    unsafe {
-        gl::Viewport(0, 0, 900, 700);
-        gl::ClearColor(0.3, 0.3, 0.5, 1.0);
-    }
-
-    'main: loop {
-        for event in event_pump.poll_iter() {
-            match event {
-                sdl2::event::Event::Quit { .. } => break 'main,
-                _ => {}
-            }
-        }
-        unsafe {
-            gl::Clear(gl::COLOR_BUFFER_BIT);
-            shader_program.set_used();
-            gl::BindVertexArray(vao);
-            gl::DrawArrays(gl::TRIANGLES, 0, 3);
-        }
-        window.gl_swap_window();
-        std::thread::sleep(std::time::Duration::new(0, 1_000_000_000u32 / 60));
-    }
 }
