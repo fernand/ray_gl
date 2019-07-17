c9fd031aae470d14da2994b5d47f95ef492ae020
Fernand Pajot <fernand@epigram.me>
2019-07-17 09:47:32 -0700

    WIP.

--- shaders/compute.glsl
+++ shaders/compute.glsl
@@ -20,11 +20,6 @@ struct Camera {
     vec3 vertical;
 };
 
-struct Ray {
-    vec3 origin;
-    vec3 direction;
-};
-
 uniform Material materials[4] = Material[4](
     Material(vec3(0.8, 0.3, 0.3), 0.0),
     Material(vec3(0.8, 0.8, 0.0), 0.0),
@@ -46,13 +41,127 @@ uniform Camera cam = Camera(
     vec3(0.2971804518378177, 0.5943609036756354, -0.2971804518378177)
 );
 
-float InterleavedGradientNoise(ivec2 xy) {
-  return fract(52.9829189f
-              * fract(xy.x * 0.06711056f
-                   + xy.y * 0.00583715f));
+uint rng_state;
+
+// Interleaved Gradient Noise to seed xorshift per pixel.
+float inoise(uvec2 xy) {
+    return fract(
+        52.9829189f * fract(xy.x * 0.06711056f + xy.y * 0.00583715f)
+    );
 }
 
+uint xorshift() {
+    rng_state ^= rng_state << 13;
+    rng_state ^= rng_state >> 17;
+    rng_state ^= rng_state << 5;
+    return rng_state;
+}
+
+float randFloat() {
+    return float(xorshift()) * (1.0 / float(0xffffffffU));
+}
+
+vec3 randInUnitSphere() {
+    while(true) {
+        vec3 p = 2.0 * vec3(xorshift(), xorshift(), xorshift()) -vec3(1.0);
+        if (dot(p, p) < 1.0) {
+            return p;
+        }
+    }
+}
+
+vec3 reflect(vec3 v, vec3 n) {
+    return v - 2.0 * dot(v, n) * n;
+}
+
+#define FLT_MAX 3.402823466e+38
+#define NUM_RAYS 100
+#define MAX_DEPTH 50
+#define NX 2000.0f
+#define NY 1000.0f
+
 void main() {
-    vec4 color = vec4(1.0, 0.0, 0.0, 1.0);
-    imageStore(pixels, ivec2(gl_GlobalInvocationID.xy), color);
+    vec3 color = vec3(0.0, 0.0, 0.0);
+    rng_state = uint(inoise(gl_GlobalInvocationID.xy) * 0xffffffffU);
+
+    for (int k=0; k<NUM_RAYS; k++) {
+        float u = (gl_GlobalInvocationID.x + randFloat()) / NX;
+        float v = (gl_GlobalInvocationID.y + randFloat()) / NY;
+        vec3 ray_origin = cam.origin;
+        vec3 ray_direction = cam.lower_left_corner + u * cam.horizontal + v * cam.vertical - cam.origin; 
+        int depth = 0;
+        vec3 attenuation = vec3(1.0);
+        while (true) {
+            if (depth >= MAX_DEPTH) {
+                // No color to add.
+                break;
+            }
+            // Collision detection with all the spheres
+            bool hit = false;
+            float hit_t = FLT_MAX;
+            vec3 hit_p;
+            vec3 hit_normal;
+            uint hit_mat_i;
+            for (int i=0; i<objects.length(); i++) {
+                vec3 oc = ray_origin - objects[i].center;
+                float a = dot(ray_direction, ray_direction);
+                float b = dot(oc, ray_direction);
+                float c = dot(oc, oc) - objects[i].radius - objects[i].radius;
+                float discriminant = b * b - a * c;
+                if (discriminant > 0.0) {
+                    float a_t_max = a * hit_t;
+                    float a_t_min = a * 0.001;
+                    float temp = -b - sqrt(discriminant);
+                    if (temp < a_t_max && temp > a_t_min) {
+                        hit = true;
+                        hit_t = temp / a;
+                        hit_p = ray_origin + hit_t * ray_direction;
+                        hit_normal = (hit_p - objects[i].center) / objects[i].radius;
+                        hit_mat_i = objects[i].mat_i;
+                        continue;
+                    }
+                    temp = -b + sqrt(discriminant);
+                    if (temp < a_t_max && temp > a_t_min) {
+                        hit = true;
+                        hit_t = temp / a;
+                        hit_p = ray_origin + hit_t * ray_direction;
+                        hit_normal = (hit_p - objects[i].center) / objects[i].radius;
+                        hit_mat_i = objects[i].mat_i;
+                        continue;
+                    }
+                }
+            }
+
+            if (hit) {
+                // Look at material.
+                Material mat = materials[hit_mat_i];
+                // Branch by metal or not metal.
+                if (mat.fuzz < 0.01) {
+                    vec3 target = hit_p + hit_normal + randInUnitSphere();
+                    ray_origin = hit_p;
+                    ray_direction = target - hit_p;
+                    attenuation *= mat.albedo;
+                } else {
+                    vec3 reflected = reflect(normalize(ray_direction), hit_normal);
+                    vec3 scattered_direction = reflected + mat.fuzz * randInUnitSphere();
+                    if (dot(scattered_direction, hit_normal) > 0.0) {
+                        ray_origin = hit_p;
+                        ray_direction = scattered_direction;
+                        attenuation *= mat.albedo;
+                    } else {
+                        break;
+                    }
+                }
+
+            } else {
+                float alpha = 0.5 * (normalize(ray_direction).y + 1.0);
+                color += attenuation * mix(vec3(1.0), vec3(0.5, 0.7, 1.0), alpha);
+                break;
+            }
+            depth += 1;
+        }
+    }
+    color /= NUM_RAYS;
+
+    imageStore(pixels, ivec2(gl_GlobalInvocationID.xy), vec4(color, 1.0));
 }
