454768054dc98d9a1dab47e081f9ebdeccf1ddf0
Fernand Pajot <fernand@epigram.me>
2019-07-22 15:09:39 -0700

    Working cpp solution.

--- build.bat
+++ build.bat
@@ -1,9 +1,10 @@
 @echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64
 
 IF NOT EXIST build mkdir build
 pushd build
 
 del *.pdb > NUL 2> NUL
 
-cl ..\main.cpp ..\include\glad\glad.c /link ..\glfw3dll.lib
+set compilerFlags=-Z7 -FC
+cl %compilerFlags% ..\main.cpp ..\include\glad\glad.c /link ..\glfw3dll.lib
