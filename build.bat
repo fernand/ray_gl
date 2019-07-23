94fba17ab390f6708d857cd9e300516db0a31d13
Fernand Pajot <fernand@epigram.me>
2019-07-22 20:21:08 -0700

    Updated build script

--- build.bat
+++ build.bat
@@ -1,5 +1,4 @@
 @echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64
 
 IF NOT EXIST build mkdir build
 pushd build
@@ -8,3 +7,5 @@ del *.pdb > NUL 2> NUL
 
 set compilerFlags=-Z7 -FC
 cl %compilerFlags% ..\main.cpp ..\include\glad\glad.c /link ..\glfw3dll.lib
+
+popd
